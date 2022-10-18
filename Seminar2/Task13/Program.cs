633f649f0c44d588563fa470005206aca1b83bbc
Aleksander <aleksandrjuk12@gmail.com>
2022-10-18 11:59:28 +0300

    Задача 13

--- Seminar2/Task13/Program.cs
+++ Seminar2/Task13/Program.cs
@@ -2,12 +2,13 @@
 int number = Convert.ToInt32(Console.ReadLine());
 int result = number;
 int n = 0;
-
-if (number > 99){
-    while (result > 0){
-        result = result / 10;
-        n++;
+// Считаем количество знаков в числе
+while (result > 0){
+    result = result / 10;
+    n++;
     }
+//Если знаков больше двух, то находим третье число    
+if (n > 2){
     int third_number = number / Convert.ToInt32(Math.Pow(10, n - 3)) % 10;
     Console.WriteLine($"Третья цифра в числе: {third_number}");
 }
