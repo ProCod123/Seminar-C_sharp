c445ae38270152d73061e6ab7b3203cc1aec880e
Aleksander <aleksandrjuk12@gmail.com>
2022-10-18 11:51:08 +0300

    Задача 13

---
+++ Seminar2/Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number;
+int n = 0;
+
+if (number > 99){
+    while (result > 0){
+        result = result / 10;
+        n++;
+    }
+    int third_number = number / Convert.ToInt32(Math.Pow(10, n - 3)) % 10;
+    Console.WriteLine($"Третья цифра в числе: {third_number}");
+}
+else{
+    Console.WriteLine($"Число слишком маленькое!");
+}
+
+
+
+
+
