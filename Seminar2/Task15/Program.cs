9380add5553e0ae3302d75aca2d6bca0f75a5e4f
Aleksander <aleksandrjuk12@gmail.com>
2022-10-18 12:25:02 +0300

    Задача 15

---
+++ Seminar2/Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 & number < 8){
+    if (number == 6 || number == 7){
+        Console.Write($"{number}-й день недели ЯВЛЯЕТСЯ ВЫХОДНЫМ");
+    }
+    if (number > 0 & number < 6){
+        Console.Write($"{number}-й день недели НЕ является выходным");
+    }
+    }
+else {
+    Console.Write($"В неделе всего 7 дней!!!");
+}
+
