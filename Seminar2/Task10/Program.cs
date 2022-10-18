609c4a125666f708aaa969aaf283b7a4fd9fa2a4
Aleksander <aleksandrjuk12@gmail.com>
2022-10-18 09:41:24 +0300

    Задача 10

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number / 100;
+int last = number % 10;
+int middle_number = (number - first * 100 - last)/10;
+Console.Write(middle_number);
