294e96481f325bf3424b66d4fc87713438dd44f6
Aleksander <aleksandrjuk12@gmail.com>
2022-10-21 11:31:27 +0300

    Задача 23

---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void GetKub(int n){
+    for(int x = 1; x <= n; x ++){
+        double kub = Math.Pow(x, 3);
+        Console.WriteLine($"Куб {x} равен {kub} ");
+    }
+}
+
+GetKub(n);
