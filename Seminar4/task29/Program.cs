a4a5dd6ed7ffe946945d7bf7f1467928290908e1
Aleksandr <aleksandrjuk12@gmail.com>
2022-11-08 22:07:02 +0300

    Задача 29

---
+++ Seminar4/task29/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите по очереди элементы массива");
+int[] array = new int[8];
+for (int i = 0 ; i < 8; i++){
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
