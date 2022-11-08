3f3818e70e95b248ab6b3fa5efed16f365f0fc3b
Aleksandr <aleksandrjuk12@gmail.com>
2022-11-08 22:05:13 +0300

    Задача 25

---
+++ Seminar4/task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите показатель степени");
+int degree = Convert.ToInt32(Console.ReadLine());
+int multiplier = number;
+
+
+for (int i = 1; i < degree; i++){
+    number = number * multiplier;
+}
+Console.WriteLine(number);
