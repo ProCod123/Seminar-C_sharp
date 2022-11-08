f4d8fb53f7cd660997fbbe67911b3412f186b299
Aleksandr <aleksandrjuk12@gmail.com>
2022-11-08 22:06:31 +0300

    Задача 27

---
+++ Seminar4/task27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < number.Length; i++){
+    sum += Convert.ToInt32(number[i].ToString());
+    }
+Console.WriteLine(sum);
