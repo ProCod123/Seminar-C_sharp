098669346def62e17e6df41db9dc5edf3efd14dd
Aleksander <aleksandrjuk12@gmail.com>
2022-11-11 10:05:34 +0300

    Задача 34

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,20 @@
+﻿//Создаем массив заполенный трехзначными числами
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(100,999);
+}
+Console.Write("Сгенерированный массив:");
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+int CountEven(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] % 2 == 0){
+            count += 1;
+        }
+    }
+    return count;
+    }
+
+int result = CountEven(array);
+Console.WriteLine($"Количество четных числе в массиве: {result}");
