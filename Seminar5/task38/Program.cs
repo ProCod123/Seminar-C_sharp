3dc626ec99641a9657a14144e2881c9fd617f990
Aleksander <aleksandrjuk12@gmail.com>
2022-11-11 10:45:22 +0300

    Задача 38

---
+++ Seminar5/task38/Program.cs
@@ -0,0 +1,25 @@
+﻿//Создаем массив заполенный вещественными числами от 0,0 до 10,0
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().NextDouble()*10;
+}
+Console.Write("Сгенерированный массив:");
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+double SubtrMaxMin(double[] array){
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i ++){
+        if (array[i] < min){
+            min = array[i];
+        }
+        if (array[i] > max){
+            max = array[i];
+        }
+    }
+    Console.WriteLine($"{min}   {max}");
+    double subtr = max - min;
+    return subtr;
+    }
+double result = SubtrMaxMin(array);
+Console.WriteLine($"Разница между максимальным и минимальным числом в массиве: {result}");
