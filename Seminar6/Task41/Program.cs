fe3f032cbc7ea0748f0ba422587a2ee3fe5dc974
Aleksander <aleksandrjuk12@gmail.com>
2022-11-15 17:33:44 +0300

    Задача 41

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Сколько чисел необходимо ввести?");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arrayPositive = new int[number];
+
+int i = 0;
+int count = 0;
+while(i < number){
+    Console.WriteLine($"Введите число № {i + 1}");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0) count ++; 
+    i ++;
+}
+
+Console.WriteLine($"Было введено {number} чисел, из них {count} положительных");
