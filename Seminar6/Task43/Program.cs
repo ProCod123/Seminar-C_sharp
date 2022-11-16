de28f5fb4ed15fdb34848a9f2537aaf5718ae828
Aleksander <aleksandrjuk12@gmail.com>
2022-11-16 11:42:19 +0300

    Задача 43

---
+++ Seminar6/Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿void CrossDot(){
+    Console.WriteLine("Введите координату точки b1");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите координату точки k1");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите координату точки b2");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите координату точки k2");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Координаты точки пересечения({x};{y})");
+}
+
+CrossDot();
