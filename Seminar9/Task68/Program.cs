dc29e82c4c24ab3c83ef062f3362843fd53846ee
Aleksander <aleksandrjuk12@gmail.com>
2022-11-25 10:39:49 +0300

    Семинар 9

---
+++ Seminar9/Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿int Akerman(int m, int n)
+            {
+                if (m == 0) return n + 1;
+                if (m != 0 && n == 0) return Akerman(m - 1, 1);
+                if (m > 0 && n > 0) return Akerman(m - 1, Akerman(m, n - 1));
+                return Akerman(m,n);
+            }
+
+
+Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());  
+
+Console.WriteLine(Akerman(m,n));
+
