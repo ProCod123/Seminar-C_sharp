864b04b2baee91f015ef499eb4605016574fc7be
Aleksander <aleksandrjuk12@gmail.com>
2022-11-25 10:43:12 +0300

    Семинар 9

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int NatureNumber(int n){
+    if (n == 1){
+        return 1;
+    }
+    Console.Write(n + ", ");
+    return NatureNumber(n - 1);
+}
+
+Console.Write($"N = {n} ->  ");
+Console.WriteLine(NatureNumber(n));
+
---
+++ Seminar9/Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int m, int n){
+    if (n == m){
+        return n;
+    }
+    
+    return m + SumNumber(m + 1, n);
+}
+
+Console.WriteLine($"Сумма чисел в диапазоне от {m} до {n} равна {SumNumber(m, n)}");
